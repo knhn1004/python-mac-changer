4d133490415a6c97ca366f51cb4d87afe5b6079b
knhn1004 <kunhan945@gmail.com>
2022-07-26 11:16:43 -0400

    complete

--- mac_changer.py
+++ mac_changer.py
@@ -34,14 +34,24 @@ def change_mac(interface, new_mac):
     subprocess.call(['ifconfig', interface, 'up'])
     
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(['ifconfig', interface])
+
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result.decode())
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print('[-] Could not read MAC address')
 
 options, arguments = get_arguments()
-# change_mac(options.interface, options.new_mac)
 
-ifconfig_result = subprocess.check_output(['ifconfig', options.interface])
+if current_mac := get_current_mac(options.interface):
+    print(f'Current MAC: {current_mac}')
 
-mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result.decode())
-if mac_address_search_result:
-    print(mac_address_search_result.group(0))
+change_mac(options.interface, options.new_mac)
+
+if new_mac := get_current_mac(options.interface):
+    if new_mac == options.new_mac:
+        print(f'[+] MAC address was successfully changed to {new_mac}')
 else:
-    print('[-] Could not read MAC address')
+    print('[-] MAC address did not get changed')
